e86811515611b56cfa87d24baa73eea84ef4651f
A771723 <vedran.ivic@atos.net>
2022-09-15 11:43:22 +0200

    Display network status

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,43 @@
 import UIKit
 import Flutter
 
+@available(iOS 12.0, *)
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
+    override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      GeneratedPluginRegistrant.register(with: self)
+      
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let networkChannel = FlutterMethodChannel(name: "com.example.moviesApp/network",
+                                                binaryMessenger: controller.binaryMessenger)
+      
+      
+      let networkMonitor: NetworkMonitor = NetworkMonitor()
+      networkMonitor.startMonitoring()
+          
+      networkChannel.setMethodCallHandler({
+          [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+              
+          guard call.method == "getNetwork" else {
+              result(FlutterMethodNotImplemented)
+              return
+              
+          }
+              
+          self?.getNetworkStatus(result: result, networkMonitor: networkMonitor)
+              
+      })
+   
+    
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func getNetworkStatus(result: FlutterResult, networkMonitor: NetworkMonitor) {
+
+            result(Bool(networkMonitor.isReachable))
+        
+    }
 }
--- ios/Runner/NetworkMonitor.swift
+++ ios/Runner/NetworkMonitor.swift
@@ -2,7 +2,41 @@
 //  NetworkMonitor.swift
 //  Runner
 //
-//  Created by Atos CVC Mac Os on 15.09.2022..
+//  Created by Atos CVC Mac Os on 14.09.2022..
 //
 
 import Foundation
+import Network
+
+@available(iOS 12.0, *)
+class NetworkMonitor {
+    static let shared = NetworkMonitor()
+
+    let monitor = NWPathMonitor()
+    private var status: NWPath.Status = .requiresConnection
+    var isReachable: Bool { status == .satisfied }
+    var isReachableOnCellular: Bool = true
+
+    func startMonitoring() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            self?.status = path.status
+            self?.isReachableOnCellular = path.isExpensive
+
+            if path.status == .satisfied {
+                print("We're connected!")
+                // post connected notification
+            } else {
+                print("No connection.")
+                // post disconnected notification
+            }
+            print(path.isExpensive)
+        }
+
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        monitor.start(queue: queue)
+    }
+
+    func stopMonitoring() {
+        monitor.cancel()
+    }
+}
